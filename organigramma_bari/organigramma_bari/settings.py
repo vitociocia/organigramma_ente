1978ced8146aec876e46ef2c952aa57452194d98
eqcola <eqcola@vodafone.it>
2025-10-16 07:48:57 +0200

    downgrade asgiref

--- organigramma_bari/organigramma_bari/settings.py
+++ organigramma_bari/organigramma_bari/settings.py
@@ -29,7 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure--bwlk2lp-atppl^-lmalg)@7ozi)g=q&wxar007)3-ya1i=h3t')
-SECRET_KEY = 'django-insecure--bwlk2lp-atppl^-lmalg)@7ozi)g=q&wxar007)3-ya1i=h3t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
